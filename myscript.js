222c2cb28a3f6e8156a3ba625569b5e5e74ceae5
benjaminelizalde <benjaminelizalde24>
2018-09-10 10:30:21 -0700

    l

--- myscript.js
+++ myscript.js
@@ -10,4 +10,46 @@ $(document).ready(function(){
 	} )
  } ) 
  
+ $(document).ready(function(){
+	$("#question3").click(function(){
+		$("#answer3").toggle();
+	} )
+ } ) 
+ 
+ $(document).ready(function(){
+	$("#question4").click(function(){
+		$("#answer4").toggle();
+	} )
+ } ) 
+ 
+ $(document).ready(function(){
+	$("#question5").click(function(){
+		$("#answer5").toggle();
+	} )
+ } ) 
+ 
+ $(document).ready(function(){
+	$("#question6").click(function(){
+		$("#answer6").toggle();
+	} )
+ } ) 
+ 
+ $(document).ready(function(){
+	$("#question7").click(function(){
+		$("#answer7").toggle();
+	} )
+ } ) 
+ 
+ $(document).ready(function(){
+	$("#question8").click(function(){
+		$("#answer8").toggle();
+	} )
+ } ) 
+ 
+ $(document).ready(function(){
+	$("#question9").click(function(){
+		$("#answer9").toggle();
+	} )
+ } ) 
+ 
  
